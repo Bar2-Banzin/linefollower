6711e59a45c4fcf7da2fdc9be44e1a9e137afd1d
Mohab <hoobamz@gmail.com>
2023-02-21 23:31:59 +0200

    MADbouly's idea (Efficient and clean code)

--- sketch_20_02.ino
+++ sketch_20_02.ino
@@ -7,7 +7,7 @@
 
 #define max_speed 80
 
-char reading = 0, prev = 0;
+car_dir reading = still, prev = still;
 
 enum motor_dir
 {
@@ -18,26 +18,16 @@ enum motor_dir
 
 enum car_dir
 {
-  still =     0,
-  forward =     0b11011,
-  forward_2 =   0b10001,
-  forward_3 =   0b10000,
-  forward_4 =   0b00001,
-  backward =  0b11111,
-  left_1 = 0b01111,
-  left_2 = 0b00111,
-  left_3 = 0b10011,
-  left_4 = 0b10111,
-  left_5 = 0b00011,
-  right_1 =     0b11110,
-  right_2 =     0b11100,
-  right_3 =     0b11001,
-  right_4 =     0b11101,
-  right_5 =     0b11000
+  still,
+  forward,
+  backward,
+  left,
+  right
 };
 
 void move_motor(char pin1, char pin2, motor_dir dir);
 void move_car(car_dir dir);
+car_dir read_line();
 
 void setup()
 {
@@ -55,13 +45,8 @@ void setup()
 void loop()
 {
   //Just a test
-  char reading = PINC & 0x1F;
-  if(prev != reading) 
-  {
-    move_car(still);
-    delay(50);
-  }
-  move_car(reading);
+  reading = read_line();
+  if(prev != reading) move_car(reading);
   delay(20);
   prev = reading;
 }
@@ -97,9 +82,6 @@ void move_car(car_dir dir)
     move_motor(IN3, IN4, m_still);
     break;
     case forward:
-    case forward_2:
-    case forward_3:
-    case forward_4:
     move_motor(IN1, IN2, m_forward);
     move_motor(IN3, IN4, m_forward);
     break;
@@ -107,21 +89,29 @@ void move_car(car_dir dir)
     move_motor(IN1, IN2, m_backward);
     move_motor(IN3, IN4, m_backward);
     break;
-    case left_1:
-    case left_2:
-    case left_3:
-    case left_4:
-    case left_5:
+    case left:
     move_motor(IN1, IN2, m_forward);
     move_motor(IN3, IN4, m_still);
     break;
-    case right_1:
-    case right_2:
-    case right_3:
-    case right_4:
-    case right_5:
+    case right:
     move_motor(IN1, IN2, m_still);
     move_motor(IN3, IN4, m_forward);
     break;
   }
+}
+
+car_dir read_line()
+{
+  char reading = PINC & 0x1F;      //Capture the sensor reading
+
+  char middle = ~reading & 0x04;   //The reading has zero in the middle
+  if(middle == 4) return forward;
+
+  char l = (reading >> 3);
+  char r = reading & 0x03;
+
+  if(l > r) return right;
+  if(r > l) return left;
+
+  return still;
 }
