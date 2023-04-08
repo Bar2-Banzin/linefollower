1bed68e293279cce95870abb339fd70f60416d62
Mohab <hoobamz@gmail.com>
2023-04-08 17:37:01 +0200

    PID with encoder

--- analogPID/analogPID.ino
+++ analogPID/analogPID.ino
@@ -13,10 +13,23 @@ int c = 1, h = 1, s = 1;
 int rpm_speeds[2] = {0, 0};
 int actual_speeds[2] = {0, 0};
 
+//Encoder variables
+int right_pulses = 0;
+int left_pulses = 0;
+int right_pulses_prev = 0;
+int left_pulses_prev = 0;
+long long start_time_l = 0;
+long long start_time_r = 0;
+int count = 1000;
+long long current_time = 0;
+long long dt = 0;
+long long encoder_resolution = pow(10,3) * (60/20);
+
+int count_print = 0;
+
 void setup()
 {
   DDRC &= ~(0x3F);
-  previousError = 0;
   pinMode(speedL, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -28,18 +41,25 @@ void setup()
   digitalWrite(IN3,HIGH);
   digitalWrite(IN4,LOW);
   Serial.begin(9600);
+  start_time_l = millis();
+  start_time_r = millis();
+  INT0_Init();
+  INT1_Init(); 
 }
 
 
 void loop()
 {
-  linefollow(0.04, 0.06);
+  linefollow(0.07, 0);
+
+  getMotorSpeeds();
   motor_ctrl(speedL, rpm_speeds[0], actual_speeds[0], 0.8, 0);
   motor_ctrl(speedR, rpm_speeds[1], actual_speeds[1], 0.6, 0);
 }
 
 void linefollow(float Kp, float Kd)
 {
+  const int base_speed_RPM = 180;
   static int previousError = 0;
   int half_error = analogRead(A4) - analogRead(A1) ;
   int side_error = analogRead(A5) - analogRead(A0) ; 
@@ -48,7 +68,7 @@ void linefollow(float Kp, float Kd)
   int P = error;
   int D = error - previousError;
 
-  float PIDvalue = (Kp * P) + (Kd * D);
+  int PIDvalue = (Kp * P) + (Kd * D);
   previousError = error;
 
   int lsp = base_speed_RPM + PIDvalue;
@@ -66,22 +86,30 @@ void linefollow(float Kp, float Kd)
   if (rsp < 0) {
     rsp = 0;
   }
-  // if(count == 2000){
-  // Serial.print("PID = ");
-  // Serial.println(PIDvalue); 
-  // Serial.print("lsp = ");
-  // Serial.println(lsp);    
-  // Serial.print("rsp = ");
-  // Serial.println(rsp);
+  if(count_print == 2000){
+  Serial.print("PID = ");
+  Serial.println(PIDvalue); 
+  Serial.print("lsp = ");
+  Serial.println(lsp);    
+  Serial.print("rsp = ");
+  Serial.println(rsp);
    
-  // Serial.print("Half = ");
-  // Serial.println(half_error);    
-  // Serial.print("Side = ");
-  // Serial.println(side_error);
+  Serial.print("Sensor_0 = ");
+  Serial.println(analogRead(A0));
+  Serial.print("Sensor_1 = ");
+  Serial.println(analogRead(A1));
+  Serial.print("Sensor_2 = ");
+  Serial.println(analogRead(A2));
+  Serial.print("Sensor_3 = ");
+  Serial.println(analogRead(A3));
+  Serial.print("Sensor_4 = ");
+  Serial.println(analogRead(A4)); 
+  Serial.print("Sensor_5 = ");
+  Serial.println(analogRead(A5));   
   // delay(2000); 
-  // count = 0;
-  // }
-  // count++;
+  count_print = 0;
+  }
+  count_print++;
 
   rpm_speeds[0] = lsp;
   rpm_speeds[1] = rsp;
@@ -91,13 +119,14 @@ void linefollow(float Kp, float Kd)
 
 void motor_ctrl(uint8_t pin, int setPoint, int actualSpeed, float Kp, float Kd)
 {
+  const int base_speed_PWM = 80;
   static int previousError = 0;
   int error = setPoint-actualSpeed; 
   
   int P = error;
   int D = error - previousError;
 
-  float PIDvalue = (Kp * P) + (Kd * D);
+  int PIDvalue = (Kp * P) + (Kd * D);
   previousError = error;
 
   int out = base_speed_PWM + PIDvalue;
@@ -109,4 +138,50 @@ void motor_ctrl(uint8_t pin, int setPoint, int actualSpeed, float Kp, float Kd)
     out = 0;
   }
   analogWrite(pin, out);
+}
+
+void getMotorSpeeds()
+{
+  current_time = millis();
+  dt = current_time - start_time_r;
+
+  long rpm_right = ((right_pulses - right_pulses_prev) * encoder_resolution) /(float)dt;
+  long rpm_left = ((left_pulses - left_pulses_prev) * encoder_resolution) /(float)dt;
+
+  if((left_pulses - left_pulses_prev) >= count){
+    left_pulses_prev = left_pulses;
+    start_time_l = millis();
+  }
+
+  if((right_pulses - right_pulses_prev) >= count){
+    right_pulses_prev = right_pulses;
+    start_time_r = millis();
+  }
+
+  actual_speeds[0] = rpm_left;
+  actual_speeds[1] = rpm_right;
+}
+
+ISR(INT0_vect) {
+  left_pulses += 1;
+}
+
+ISR(INT1_vect) {
+  right_pulses += 1;
+}
+
+void INT0_Init (void) {
+  SREG &= ~(1<<7);
+  DDRD &= (~(1<<PD2));
+  EIMSK |= (1<<INT0);
+  EICRA |= (1<<ISC00) | (1<<ISC01);
+  SREG |= (1<<7);
+}
+
+void INT1_Init (void) {
+  SREG &= ~(1<<7);
+  DDRD &= (~(1<<PD3));
+  EIMSK |= (1<<INT1);
+  EICRA |= (1<<ISC10) | (1<<ISC11);
+  SREG |= (1<<7);
 }
