d8a01a53fc31fbcfb4208fee3b56c0ee355c0c6a
emanshahda <eman.shahda01@eng-st.cu.edu.eg>
2023-04-16 14:33:42 +0200

    Car is battery dependent

--- openloopPID/openloopPID.ino
+++ openloopPID/openloopPID.ino
@@ -2,24 +2,39 @@
  Sample Line Following Code for the Robojunkies LF-2 robot
 */
 
-#define speedL 10
+// Speed 100
+//  kp =0.055
+// kd=0.15
+
+//Speed 120 - BATTERIES: 3.95 3.85
+//kp = 0.065
+//kd = 0.28
+
+// #define speedL 10
+// #define IN1 7
+// #define IN2 6
+// #define IN3 9
+// #define IN4 8
+// #define speedR 11
+
+#define speedL 5
 #define IN1 7
-#define IN2 6
+#define IN2 8
 #define IN3 9
-#define IN4 8
-#define speedR 11
+#define IN4 10
+#define speedR 6
 
 int P, D, previousError, PIDvalue, error;
 int lsp, rsp;
-int lfspeed = 100;
+int lfspeed = 140;
 
 float Kp = 0;
 float Kd = 0;
 
 void setup()
 {
-  Kp = 0.055;
-  Kd = 0;
+  Kp = 0.065;
+  Kd = 0.28;
   DDRC &= ~(0x1F);
   previousError = 0;
   pinMode(speedL, OUTPUT);
@@ -32,6 +47,7 @@ void setup()
   digitalWrite(IN2,HIGH);
   digitalWrite(IN3,HIGH);
   digitalWrite(IN4,LOW);
+  // Serial.begin(9600);
 }
 
 
@@ -46,7 +62,8 @@ void loop()
 void linefollow()
 {
   int error = (analogRead(A0)+analogRead(A1)) - (analogRead(A4) + analogRead(A3));
-
+  // Serial.println(error);
+  // delay(1000);
   P = error;
   D = error - previousError;
 
@@ -56,18 +73,11 @@ void linefollow()
   lsp = lfspeed + PIDvalue;
   rsp = lfspeed - PIDvalue;
 
-  if (lsp > 255) {
-    lsp = 255;
-  }
-  if (lsp < 0) {
-    lsp = 0;
-  }
-  if (rsp > 255) {
-    rsp = 255;
-  }
-  if (rsp < 0) {
-    rsp = 0;
-  }
+  if (lsp > 255) lsp = 255;
+  if (lsp < 0)   lsp = 0;
+  if (rsp > 255) rsp = 255;
+  // if (rsp < 80)  rsp = 70;
+  if (rsp < 0)   rsp = 0;
   analogWrite(speedL, lsp);
   analogWrite(speedR, rsp);
 }
